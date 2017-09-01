769512f0e6a7e7656067d1a081625411c90ae0b4
carotene512 <karoten512@gmail.com>
2017-09-01 16:56:35 +0900

    routing成功。

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+
+import { SecondPageComponent } from './second-page/second-page.component';
+import { FirstPageComponent } from './first-page/first-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SecondPageComponent,
+    FirstPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: "first", component: FirstPageComponent },
+      {path: "second", component: SecondPageComponent },
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
